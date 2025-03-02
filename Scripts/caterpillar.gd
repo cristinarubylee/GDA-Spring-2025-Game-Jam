cf56189671189328f3b840258518a5f7f86d66f8
Cristina Lee <cristinalee0723@gmail.com>
2025-03-01 21:11:01 -0500

    Caterpillar sprite and better movement
    - Changed caterpillar movement to not be based off rays anymore
    - Sprite flips depending on direction
    - Still somewhat buggy??

--- Scripts/caterpillar.gd
+++ Scripts/caterpillar.gd
@@ -1,49 +1,81 @@
 extends CharacterBody2D
-
 var speed := 500
-var jump := -1000
+var gravity := 40 * 980
 var isClimbing := false
-@onready var ground_ray = $GroundRay
-@onready var l_wall_ray = $LeftWallRay
-@onready var r_wall_ray = $RightWallRay
+var prev_isClimbing := false  # Track previous climbing state
+var wall_detect_distance := 5.0  # Distance to check for walls
+var wall_offset := 50.0  # Vertical offset from ground when first climbing (avoid clipping)
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	var surface_normal = Vector2.UP # Default to ground movement
-	
-	# Detect walls and ground
+	# Default to ground movement
+	var surface_normal = Vector2.UP
+	prev_isClimbing = isClimbing
 	isClimbing = false
-	if l_wall_ray.is_colliding():
-		surface_normal = l_wall_ray.get_collision_normal()
-		isClimbing = true
-	elif r_wall_ray.is_colliding():
-		surface_normal = r_wall_ray.get_collision_normal()
-		isClimbing = true
-	elif ground_ray.is_colliding():
-		surface_normal = ground_ray.get_collision_normal()
 	
-	# Check to see if trying to get back to ground
+	var left_collision = move_and_collide(Vector2.LEFT * wall_detect_distance, true)
+	var right_collision = move_and_collide(Vector2.RIGHT * wall_detect_distance, true)
+	
+	if left_collision:
+		surface_normal = left_collision.get_normal()
+		isClimbing = true
+		
+		if !prev_isClimbing:
+			var wall_position = left_collision.get_position()
+			var offset_vector = Vector2.UP * wall_offset
+			global_position = wall_position + offset_vector
+			
+	elif right_collision:
+		surface_normal = right_collision.get_normal()
+		isClimbing = true
+		
+		if !prev_isClimbing:
+			var wall_position = right_collision.get_position()
+			var offset_vector = Vector2.UP * wall_offset
+			global_position = wall_position + offset_vector
+			
+	elif is_on_floor():
+		surface_normal = get_floor_normal()
+	
+	# Check if trying to get back to ground
 	if is_on_floor() and isClimbing:
-		if l_wall_ray.is_colliding() and Input.is_action_just_pressed("ui_right"):
+		if left_collision and Input.is_action_pressed("ui_right"):
+			# Left wall and moving right
 			isClimbing = false
-			surface_normal = ground_ray.get_collision_normal()
-		elif r_wall_ray.is_colliding() and Input.is_action_just_pressed("ui_left"):
+			surface_normal = get_floor_normal()
+		elif right_collision and Input.is_action_pressed("ui_left"):
+			# Right wall and moving left
 			isClimbing = false
-			surface_normal = ground_ray.get_collision_normal()
+			surface_normal = get_floor_normal()
 	
 	# Compute movement direction perpendicular to the surface normal
 	var move_dir = surface_normal.rotated(PI / 2)
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Rotate the entire node (including collider) to align with surface
+	# If on ground, no change!
+	var angle = Vector2.UP.angle_to(surface_normal)
+	rotation = angle
+	
+	# Flip sprite based on direction
+	if direction < 0:
+		sprite.flip_h = true
+	elif direction > 0:
+		sprite.flip_h = false
+	
 	velocity = move_dir * speed * direction
 	
 	# Allow for up and down arrow keys when climbing
 	if isClimbing:
 		if Input.is_action_pressed("ui_up"):
 			velocity = Vector2.UP * speed
+			sprite.flip_h = false
 		elif Input.is_action_pressed("ui_down"):
 			velocity = Vector2.DOWN * speed
+			sprite.flip_h = true
 	
+	# Apply gravity
 	if not is_on_floor() and not isClimbing:
-		velocity += 40 * get_gravity() * delta
-	
+		velocity.y += gravity * delta
 	
 	move_and_slide()
