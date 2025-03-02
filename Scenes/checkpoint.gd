da78757a2181f8d12c6155fda6d70c2a083e079c
cristinarubylee <cristina0723@gmail.com>
2025-03-02 17:33:30 -0500

    Fixed butterfly checkpoint

--- Scenes/checkpoint.gd
+++ Scenes/checkpoint.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
 @onready var camera = %stuck_camera
+@onready var caterpillar = $Caterpillar
 @onready var butterfly = $Butterfly
 
 @export var state = 0
 
 func body_entered(body: Node2D) -> void:
+	if body.name == "Egg" and state == 0:
+		caterpillar.position = body.position
+		caterpillar.visible = true
+		body.queue_free()
 	if body.name == "Caterpillar" and state == 1:
 		butterfly.position = body.position
 		butterfly.visible = true
